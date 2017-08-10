11f6df42965bb638b05537b398ecb4e31b3d6182
Nam Nguyen <nam040494@gmail.com>
2017-08-10 16:15:30 +0700

    pull 2-chap 5

--- spec/controllers/static_page_controller_spec.rb
+++ spec/controllers/static_page_controller_spec.rb
@@ -1,30 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-	describe "static_pages/home" do
-		it "test controller home" do
-			get :home
-			expect(response.status).to eq(200)
-		end
-	end
 
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #help" do
+    it "returns http success" do
+      get :help
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  context "index title home" do
+    render_views
+    before do
+      get :show, params: {page: :home}
+    end
+    it{expect(response).to render_template :home}
+    it{expect(response.body).to have_title("Home | Ruby on Rails Tutorial Sample App")}
+  end
+
+  context "index title help" do
+    render_views
+    before do
+      get :show, params: {page: :help}
+    end
+    it{expect(response).to render_template :help}
+    it{expect(response.body).to have_title("Help | Ruby on Rails Tutorial Sample App")}
+  end
+
+  context "index title about" do
+    render_views
+    before do
+      get :show, params: {page: :about}
+    end
+    it{expect(response).to render_template :about}
+    it{expect(response.body).to have_title("About | Ruby on Rails Tutorial Sample App")}
+  end
 end
-RSpec.describe StaticPagesController, type: :controller do
-	describe "static_pages/help" do
-		it "test controller help" do
-			get :help
-			expect(response.status).to eq(200)
-		end
-	end
-
-end
-RSpec.describe StaticPagesController, type: :controller do
-	describe "static_pages/about" do
-		it "test controller about" do
-			get :about
-			expect(response.status).to eq(200)
-		end
-	end
-
-end
-
